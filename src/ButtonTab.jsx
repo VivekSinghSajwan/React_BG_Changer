8bafaad1732a968591ab4231087900f15fd970ff
VivekSinghSajwan <vssajwan09123@gmail.com>
2025-04-22 18:19:05 +0530

    format code

--- src/ButtonTab.jsx
+++ src/ButtonTab.jsx
@@ -9,13 +9,13 @@ function ButtonTab(){
     return (
         <div className="body w-full h-screen py-4" style={{ backgroundColor: color }}>
             <div className="w-5xl m-auto bg-white px-1 py-3 mb-50 flex justify-evenly rounded-full">
-                <button onClick={() => changeColor('red')} className="text-white rounded-xl bg-red-400 px-7" >RED</button>
-                <button onClick={() => changeColor('orange')}className="text-white rounded-xl bg-orange-400 px-7">ORANGE</button>
-                <button onClick={() => changeColor('yellow')} className="text-white rounded-xl bg-yellow-400 px-7">YELLOW</button>
-                <button onClick={() => changeColor('green')} className="text-white rounded-xl bg-green-400 px-7">GREEN</button>
-                <button onClick={() => changeColor('blue')} className="text-white rounded-xl bg-blue-400 px-7">BLUE</button>
-                <button onClick={() => changeColor('indigo')} className="text-white rounded-xl bg-indigo-400 px-7">INDIGO</button>
-                <button onClick={() => changeColor('violet')} className="text-white rounded-xl bg-violet-400 px-7">VIOLET</button>
+                <button onClick={() => changeColor('red')}    className="text-white rounded-xl px-7 bg-red-400" >RED</button>
+                <button onClick={() => changeColor('orange')} className="text-white rounded-xl px-7 bg-orange-400">ORANGE</button>
+                <button onClick={() => changeColor('yellow')} className="text-white rounded-xl px-7 bg-yellow-400">YELLOW</button>
+                <button onClick={() => changeColor('green')}  className="text-white rounded-xl px-7 bg-green-400">GREEN</button>
+                <button onClick={() => changeColor('blue')}   className="text-white rounded-xl px-7 bg-blue-400">BLUE</button>
+                <button onClick={() => changeColor('indigo')} className="text-white rounded-xl px-7 bg-indigo-400">INDIGO</button>
+                <button onClick={() => changeColor('violet')} className="text-white rounded-xl px-7 bg-violet-400">VIOLET</button>
             </div>
         </div>
     )
